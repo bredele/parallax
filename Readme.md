3451fa70445084b99264ffdf828691696cebd121
bredele <olivier.wietrich@gmail.com>
2013-07-02 03:44:21 -0600

    frist dragt

---
+++ Readme.md
@@ -0,0 +1,29 @@
+
+# parallax
+
+  Simple parallax handler
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install bredele/parallax
+
+## API
+
+### Parallax#at(index,callback,[scope])
+
+  Execute callback with optional scope at specific scroll events (indexY).
+
+### Parallax#on(callback,[scope])
+
+  Execute callback with optional scope for every scroll events.
+
+## Todo
+
+  - Use Request animation frame
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,54 @@
+var raf = require('raf')
+  , bind = require('bind')
+  , Emitter = require('emitter')
+  , emitter = new Emitter();
+
+
+window.onscroll = function(){
+  //emit event for every instance of Parallax
+  var y = window.scrollY;
+  emitter.emit(y);
+  emitter.emit('scroll', y);
+};
+
+
+
+/**
+ * Expose 'Parallax'
+ */
+
+module.exports = Parallax;
+
+
+/**
+ * Initialize Window Vertical Parallax
+ *
+ */
+
+function Parallax() {
+  
+}
+
+/**
+ * Trigger action on specific scroll events
+ *
+ * @param {Number} index scroll's index
+ * @param {Function} callback function
+ * @param {Object} scope optional callback' scope
+ */
+
+Parallax.prototype.at = function(index, callback, scope) {
+  emitter.on(index, bind(callback, scope));
+};
+
+
+/**
+ * Trigger action on every scroll events
+ *
+ * @param {Function} callback function
+ * @param {Object} scope optional callback' scope
+ */
+
+Parallax.prototype.on = function(callback, scope) {
+  emitter.on('scroll', bind(callback, scope));
+};
